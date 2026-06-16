195a2e787a4d25017e64e3da6218b647481f8dd7
diddy626262 <diddy626262@users.noreply.github.com>
2026-06-16 12:12:40 +0000

    fix: Add poll(timeout) and offer(timeout) to ArrayBlockingQueue

--- sources/teavm/patch/java/java/util/concurrent/ArrayBlockingQueue.java
+++ sources/teavm/patch/java/java/util/concurrent/ArrayBlockingQueue.java
@@ -13,4 +13,6 @@ public class ArrayBlockingQueue<E> extends java.util.AbstractQueue<E> implements
     public int drainTo(java.util.Collection<? super E> c) { int n = 0; while (!queue.isEmpty()) { c.add(queue.poll()); n++; } return n; }
     public int drainTo(java.util.Collection<? super E> c, int maxElements) { int n = 0; while (n < maxElements && !queue.isEmpty()) { c.add(queue.poll()); n++; } return n; }
     public int remainingCapacity() { return Integer.MAX_VALUE; }
+    public E poll(long timeout, TimeUnit unit) throws InterruptedException { return poll(); }
+    public boolean offer(E e, long timeout, TimeUnit unit) throws InterruptedException { return offer(e); }
 }
