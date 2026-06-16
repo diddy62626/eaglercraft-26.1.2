b57c4706b2e04783557b54277ad8c8321f802dd9
Z User <z@container>
2026-06-16 18:51:39 +0000

    fix: same issue in KQueue.ensureAvailability

--- sources/teavm/java/io/netty/channel/kqueue/KQueue.java
+++ sources/teavm/java/io/netty/channel/kqueue/KQueue.java
@@ -2,6 +2,6 @@ package io.netty.channel.kqueue;
 
 public final class KQueue {
     public static boolean isAvailable() { return false; }
-    public static boolean ensureAvailability() {}
+    public static boolean ensureAvailability() { return false; }
     public static Throwable unavailabilityCause() { return new UnsupportedOperationException("KQueue not available in browser"); }
 }
