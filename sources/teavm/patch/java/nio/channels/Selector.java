a3c5ef4c39b69119ac8b01afbd1a99566573fb91
Z User <z@container>
2026-06-16 19:12:10 +0000

    fix: make Selector an interface (so AbstractSelector can implements Selector)

--- sources/teavm/patch/java/nio/channels/Selector.java
+++ sources/teavm/patch/java/nio/channels/Selector.java
@@ -5,20 +5,25 @@ import java.io.IOException;
 import java.nio.channels.spi.SelectorProvider;
 import java.util.Set;
 
-public abstract class Selector implements Closeable {
-    protected Selector() {}
-
-    public static Selector open() throws IOException {
+/**
+ * TeaVM stub for java.nio.channels.Selector.
+ * Declared as interface so that AbstractSelector can `implements Selector`.
+ *
+ * (Real Java has Selector as abstract class, but our stub uses interface
+ * to avoid Java's single-inheritance restriction with AbstractInterruptibleChannel.)
+ */
+public interface Selector extends Closeable {
+    static Selector open() throws IOException {
         return null; // Browser: no selector support
     }
 
-    public abstract boolean isOpen();
-    public abstract SelectorProvider provider();
-    public abstract Set<SelectionKey> keys();
-    public abstract Set<SelectionKey> selectedKeys();
-    public abstract int selectNow() throws IOException;
-    public abstract int select(long timeout) throws IOException;
-    public abstract int select() throws IOException;
-    public abstract Selector wakeup();
-    @Override public abstract void close() throws IOException;
+    boolean isOpen();
+    SelectorProvider provider();
+    Set<SelectionKey> keys();
+    Set<SelectionKey> selectedKeys();
+    int selectNow() throws IOException;
+    int select(long timeout) throws IOException;
+    int select() throws IOException;
+    Selector wakeup();
+    @Override void close() throws IOException;
 }
