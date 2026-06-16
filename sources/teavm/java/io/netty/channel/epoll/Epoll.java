5f2f8a4c3f318dc66c1a6e3c54f6a54c0e533d53
Z User <z@container>
2026-06-16 18:51:33 +0000

    fix: Epoll.ensureAvailability() missing return statement

--- sources/teavm/java/io/netty/channel/epoll/Epoll.java
+++ sources/teavm/java/io/netty/channel/epoll/Epoll.java
@@ -2,6 +2,6 @@ package io.netty.channel.epoll;
 
 public final class Epoll {
     public static boolean isAvailable() { return false; }
-    public static boolean ensureAvailability() {}
+    public static boolean ensureAvailability() { return false; }
     public static Throwable unavailabilityCause() { return new UnsupportedOperationException("Epoll not available in browser"); }
 }
