3622fae124493cd0e4fd5bb6144212cfb875969b
Z User <z@container>
2026-06-17 08:18:06 +0000

    fix: BlendFunction constructor(SourceFactor, DestFactor, SourceFactor, DestFactor) + SourceFactor/DestFactor enums

--- sources/teavm/java/com/mojang/blaze3d/pipeline/BlendFunction.java
+++ sources/teavm/java/com/mojang/blaze3d/pipeline/BlendFunction.java
@@ -1,5 +1,23 @@
 package com.mojang.blaze3d.pipeline;
 
-public enum BlendFunction {
-    NONE, TRANSPARENT, TRANSLUCENT, ADD, ADDITIVE, SUBTRACT, MULTIPLY, SCREEN, OVERLAY, GLINT;
+import com.mojang.blaze3d.platform.SourceFactor;
+import com.mojang.blaze3d.platform.DestFactor;
+
+public class BlendFunction {
+    public static final BlendFunction NONE = new BlendFunction();
+    public static final BlendFunction TRANSPARENT = new BlendFunction();
+    public static final BlendFunction TRANSLUCENT = new BlendFunction();
+    public static final BlendFunction ADD = new BlendFunction();
+    public static final BlendFunction ADDITIVE = new BlendFunction();
+    public static final BlendFunction SUBTRACT = new BlendFunction();
+    public static final BlendFunction MULTIPLY = new BlendFunction();
+    public static final BlendFunction SCREEN = new BlendFunction();
+    public static final BlendFunction OVERLAY = new BlendFunction();
+    public static final BlendFunction GLINT = new BlendFunction();
+
+    public BlendFunction() {}
+
+    public BlendFunction(SourceFactor srcColor, DestFactor dstColor, SourceFactor srcAlpha, DestFactor dstAlpha) {
+        // Store factors for WebGL blend configuration
+    }
 }
