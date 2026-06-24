21c6a271d083f1aaeeef8bc1b557197409ee5a00
Z User <z@container>
2026-06-24 23:43:23 +0000

    fix: use empty anonymous MapCodec (interface has all default methods)
    MapCodec is an interface with all default methods, so an empty
    anonymous class 'new MapCodec<Object>() {}' works as a no-op.
    
    This compiles cleanly and provides a functional no-op MapCodec.

--- sources/teavm/java/com/mojang/serialization/codecs/RecordCodecBuilder.java
+++ sources/teavm/java/com/mojang/serialization/codecs/RecordCodecBuilder.java
@@ -2,10 +2,7 @@ package com.mojang.serialization.codecs;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
-import com.mojang.serialization.DataResult;
-import com.mojang.serialization.DynamicOps;
 import java.util.function.Function;
-import java.util.stream.Stream;
 
 // PATCHED: Returns no-op Codec/MapCodec instances instead of null.
 
@@ -18,36 +15,19 @@ public class RecordCodecBuilder {
     
     @SuppressWarnings("unchecked")
     public static <O> MapCodec<O> mapCodec(Function<O, MapCodec<O>> builder) {
-        return (MapCodec<O>) NoOpMapCodec.INSTANCE;
+        return (MapCodec<O>) EMPTY_MAPCODEC;
     }
     
     @SuppressWarnings("unchecked")
     public static <O, F> MapCodec<F> forGetter(Function<O, F> getter) {
-        return (MapCodec<F>) NoOpMapCodec.INSTANCE;
+        return (MapCodec<F>) EMPTY_MAPCODEC;
     }
     
     @SuppressWarnings("unchecked")
     public static <O, F> MapCodec<F> of(MapCodec<F> codec, Function<O, F> getter) {
-        return (MapCodec<F>) NoOpMapCodec.INSTANCE;
+        return (MapCodec<F>) EMPTY_MAPCODEC;
     }
     
-    // Simple no-op MapCodec implementation
-    private static class NoOpMapCodec<T> extends MapCodec<T> {
-        static final NoOpMapCodec<Object> INSTANCE = new NoOpMapCodec<>();
-        
-        @Override
-        public <T2> DataResult<T> decode(DynamicOps<T2> ops, com.mojang.serialization.MapLike<T2> input) {
-            return DataResult.success(null);
-        }
-        
-        @Override
-        public <T2> DataResult<T2> encode(T input, DynamicOps<T2> ops, T2 prefix) {
-            return DataResult.success(prefix);
-        }
-        
-        @Override
-        public <T2> Stream<T2> keys(DynamicOps<T2> ops) {
-            return Stream.empty();
-        }
-    }
+    // MapCodec is an interface with all default methods, so empty impl works
+    private static final MapCodec<?> EMPTY_MAPCODEC = new MapCodec<Object>() {};
 }
