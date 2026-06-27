c570c1a483ca7f9c3a8df8e6b4fab291e4e197b1
Z User <z@container>
2026-06-27 12:10:25 +0000

    fix: add FileFixStats.getProgress() -> float

--- sources/teavm/java/net/minecraft/util/worldupdate/UpgradeProgress.java
+++ sources/teavm/java/net/minecraft/util/worldupdate/UpgradeProgress.java
@@ -2,6 +2,7 @@ package net.minecraft.util.worldupdate;
 public class UpgradeProgress {
     public static class FileFixStats {
         public int totalOperations() { return 0; }
+        public float getProgress() { return 0f; }
     }
     public FileFixStats getTotalFileFixStats() { return null; }
     public FileFixStats getTypeFileFixStats() { return null; }
