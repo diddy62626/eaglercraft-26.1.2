8282a3604c2cb08778f019be3d9d97761a8b270f
Z User <z@container>
2026-06-27 11:15:13 +0000

    fix: add LevelStorageSource.parseValidator(Path) stub
    CI #457 got past all infrastructure issues (JDK 26 javac NPE, class
    file version 70, ASM version check, DFU corruption). TeaVM is now
    actually analyzing the MC constructor's call graph!
    
    First missing method: LevelStorageSource.parseValidator(Path) returns
    DirectoryValidator. Called from Minecraft.<init> at line 442.
    Added as a stub returning null.

--- sources/teavm/java/net/minecraft/world/level/storage/LevelStorageSource.java
+++ sources/teavm/java/net/minecraft/world/level/storage/LevelStorageSource.java
@@ -1,4 +1,8 @@
 package net.minecraft.world.level.storage;
+import java.nio.file.Path;
 public class LevelStorageSource {
     public static class LevelStorageAccess {}
+    public static Object parseValidator(Path path) {
+        return null;
+    }
 }
