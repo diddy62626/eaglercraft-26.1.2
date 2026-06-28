1ab194631ba3ec39dd9c301b5dd1533ea93a00ea
Z User <z@container>
2026-06-28 00:44:09 +0000

    fix: add missing import for java.nio.file.spi.FileSystemProvider
    CI #475 error: 'cannot find symbol' on 'extends FileSystemProvider'
    because FileSystemProvider is in java.nio.file.spi, not java.nio.file.
    The wildcard import java.nio.file.* doesn't cover sub-packages.

--- sources/teavm/java/net/lax1dude/eaglercraft/v2_6/patch/JarFileSystemProvider.java
+++ sources/teavm/java/net/lax1dude/eaglercraft/v2_6/patch/JarFileSystemProvider.java
@@ -6,6 +6,7 @@ import java.net.URI;
 import java.nio.channels.SeekableByteChannel;
 import java.nio.file.*;
 import java.nio.file.attribute.*;
+import java.nio.file.spi.FileSystemProvider;
 import java.util.Map;
 import java.util.Set;
 
